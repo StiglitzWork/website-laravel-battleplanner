89c31040a58dc5b602bf3e7c479c75388704626e
Jared Snape <jaredsnape@gmail.com>
2018-11-16 21:46:26 -0500

    Fix 404 issues with op icons and seperate icon display by secondary and general gadget types

--- resources/views/room/sidebar-icons.blade.php
+++ resources/views/room/sidebar-icons.blade.php
@@ -1,22 +1,13 @@
+<h2>Secondary gadgets:</h2>
 @foreach ($gadgets as $key => $gadget)
+  @if(!$gadget->prime && !$gadget->general)
     <img src="{{$gadget->icon}}" draggable="true" ondragstart="app.engine.drag(event)"  alt="" height="50px" width="50px">
+    @endif
 @endforeach
 
-{{-- <div class="row">
-    <div class="col-12">
-        @if (Auth::user() == $room->Owner)
-            @if ($room->battleplan && $room->battleplan->notes)
-                <textarea class="form-control battleplan_notes" id="battleplan_notes">{{$room->battleplan->notes}}</textarea>
-            @else
-                <textarea class="form-control battleplan_notes" id="battleplan_notes"></textarea>
-            @endif
-        @else
-            @if ($room->battleplan && $room->battleplan->notes)
-                <textarea class="form-control battleplan_notes" id="battleplan_notes" disabled>{{$room->battleplan->notes}}</textarea>
-            @else
-                <textarea class="form-control battleplan_notes" id="battleplan_notes" disabled></textarea>
-            @endif
-        @endif
-    </div>
-</div> --}}
-
+<h2>General tools:</h2>
+@foreach ($gadgets as $key => $gadget)
+  @if(!$gadget->prime && $gadget->general)
+    <img src="{{$gadget->icon}}" draggable="true" ondragstart="app.engine.drag(event)"  alt="" height="50px" width="50px">
+    @endif
+@endforeach
