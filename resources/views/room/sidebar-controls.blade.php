52a898c54ce51125ada3b912c33e189a4b877dc8
Jared Snape <jaredsnape@gmail.com>
2018-12-06 18:06:49 -0500

    Fix sidebar CSS

--- resources/views/room/sidebar-controls.blade.php
+++ resources/views/room/sidebar-controls.blade.php
@@ -2,21 +2,21 @@
 @if ($room->Owner == Auth::User())
     <div class="row margin10">
         <div class="col-6">
-                <button type="button" name="button" class="btn btn-info" data-toggle="modal"
-                    data-target="#mapModal">Load</button>
+                <button type="button" name="button" class="btn btn-info sidebar-btn" data-toggle="modal"
+                    data-target="#mapModal">Load/New</button>
         </div>
         <div class="col-6">
-                <button type="button" name="button" class="btn btn-success" onclick="app.engine.save()">Save</button>
+                <button type="button" name="button" class="btn btn-success sidebar-btn" onclick="app.engine.save()">Save</button>
         </div>
     </div>
 @endif
 <div class="row margin10">
     <div class="col-6">
-            <button type="button" name="button" class="btn btn-primary" onclick="app.engine.changeFloor(-1)" data-toggle="tooltip" data-placement="top" title="Keybind: Down arrow">Floor
+            <button type="button" name="button" class="btn btn-primary sidebar-btn" onclick="app.engine.changeFloor(-1)" data-toggle="tooltip" data-placement="top" title="Keybind: Down arrow">Floor
                 &darr;</button>
     </div>
-        <div class="col-6">
-                <button type="button" name="button" class="btn btn-primary" onclick="app.engine.changeFloor(1)" data-toggle="tooltip" data-placement="top" title="Keybind: Up arrow">Floor
-                    &uarr;</button>
+    <div class="col-6">
+            <button type="button" name="button" class="btn btn-primary sidebar-btn" onclick="app.engine.changeFloor(1)" data-toggle="tooltip" data-placement="top" title="Keybind: Up arrow">Floor
+                &uarr;</button>
     </div>
 </div>
