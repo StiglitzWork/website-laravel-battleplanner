acb4088dd7ec073a4795e4c8a83eb4a9bca9ae47
Erik Smith <erik.aaron.smith@gmail.com>
2018-12-10 20:41:11 -0500

    Google analytics + improved battleplan show

--- resources/views/battleplan/show.blade.php
+++ resources/views/battleplan/show.blade.php
@@ -4,12 +4,13 @@
 
 {{-- init --}}
 <script type="text/javascript">
-    const BATTLEPLAN_ID = {{$battleplan->id}};
+    const BATTLEPLAN_ID = {{ $battleplan->id}};
+
 </script>
 
 <script src="{{r_asset("js/battleplan/show.bundle.js")}}"></script>
 
-  <script>
+<script>
     $(document).ready(function() {
         $('[data-toggle="tooltip"]').tooltip();
     });
@@ -42,7 +43,7 @@
 
         success: function (result) {
             console.log(result);
-            $("#vote-value-" + battleplanId ).html("Points: " + result);
+            $("#vote-value-" + battleplanId ).html(result);
         },
 
         error: function (result) {
@@ -81,73 +82,184 @@
 @endpush
 
 @push('css')
-  <link rel="stylesheet" href="{{r_asset("css/battleplan/show.css")}}">
+<link rel="stylesheet" href="{{r_asset("css/battleplan/show.css")}}">
 @endpush
 
 @section('content')
-  
-  <div class="row">
-    <div class="col-12 text-center">
-        <h1>Battleplan: {{$battleplan->name}}</h1>
-        <h2>by: {{$battleplan->Owner->username}}</h2>
-        <small id="vote-value-{{$battleplan->id}}">Points: {{$battleplan->voteSum()}}</small>
-    </div>
-  </div>
 
-  @if (!$battleplan->public)
-    <div class="row">
-        <div class="col-12 text-center">
-            <small style="color:red">This battleplan is private and is only visible to the owner</small>
+{{-- New --}}
+<div class="row">
+    <div class="accordion col-12" id="accordionExample">
+        <div class="card">
+            <div class="card-header" id="headingOne">
+                <h5 class="mb-0">
+                    <button class="btn btn-info col-12 text-center" type="button" data-toggle="collapse" data-target="#collapseOne"
+                        aria-expanded="true" aria-controls="collapseOne">
+                        Information
+                    </button>
+                </h5>
+            </div>
+            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample"
+                style="">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-12 col-xl-6 text-center">
+                            <table class="table table-bordered">
+                                <tbody>
+                                    <tr>
+                                        <td>Battle Plan</td>
+                                        <td><strong>{{$battleplan->name}}</strong></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Creator</td>
+                                        <td>{{$battleplan->Owner->username}}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Points</td>
+                                        <td id="vote-value-{{$battleplan->id}}">{{$battleplan->voteSum()}}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Functions</td>
+                                        <td>
+                                                @if(Auth::user())
+                                                @if ($battleplan->voted(1))
+                                                <i class="fas fa-arrow-circle-up cursor-click vote-green" id="vote-up-{{$battleplan->id}}" onclick="vote(1,{{$battleplan->id}}, this)"
+                                                    data-toggle="tooltip" data-placement="top" title="Up Vote"></i>
+                                                @else
+                                                <i class="fas fa-arrow-circle-up cursor-click" id="vote-up-{{$battleplan->id}}" onclick="vote(1,{{$battleplan->id}}, this)"
+                                                    data-toggle="tooltip" data-placement="top" title="Up Vote"></i>
+                                                @endif
+                                        
+                                                |
+                                        
+                                                @if ($battleplan->voted(-1))
+                                                <i class="fas fa-arrow-circle-down cursor-click vote-red" id="vote-down-{{$battleplan->id}}" onclick="vote(-1,{{$battleplan->id}}, this)"
+                                                    data-toggle="tooltip" data-placement="top" title="Down Vote"></i>
+                                                @else
+                                                <i class="fas fa-arrow-circle-down cursor-click" id="vote-down-{{$battleplan->id}}" onclick="vote(-1,{{$battleplan->id}}, this)"
+                                                    data-toggle="tooltip" data-placement="top" title="Down Vote"></i>
+                                                @endif
+                                        
+                                                |
+                                        
+                                                <i class="fas fa-clone cursor-click" id="copy-{{$battleplan->id}}" data-toggle="tooltip" data-placement="top"
+                                                    title="Copy to my account" onclick="copyModal({{$battleplan->id}})"></i>
+                                        
+                                                @else
+                                                <small style="color:red">Please sign in to unlock more features</small>
+                                                @endif
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <div class="col-md-12 col-xl-6 text-center">
+                            <table class="table table-bordered">
+                                <tbody>
+                                    <tr>
+                                        <td>Status</td>
+
+                                        @if ($battleplan->public)
+                                            <td  style="color:green">
+                                                Public
+                                            </td>
+                                        @else
+                                            <td style="color:red">
+                                                Private <br>
+                                                <small>This battleplan is private and is only visible to the owner</small>
+                                            </td>
+                                        @endif
+                                    </tr>
+                                    
+                                    <tr>
+                                        <td>Created</td>
+                                        <td>{{$battleplan->created_at}}</td>
+                                    </tr>
+        
+                                    <tr>
+                                        <td>Notes</td>
+                                        <td>
+                                            <textarea disabled="" style="width: 100%">{{$battleplan->notes}}</textarea>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     </div>
-  @endif
+</div>
+</div>
 
+{{--
+@if (!$battleplan->public)
+<div class="row">
+    <div class="col-12 text-center">
+        <small style="color:red">This battleplan is private and is only visible to the owner</small>
+    </div>
+</div>
+@endif --}}
 
+{{--
 <div class="row">
     <div class="col-12 text-center">
 
         @if(Auth::user())
-            @if ($battleplan->voted(1))
-                <i class="fas fa-arrow-circle-up cursor-click vote-green" id="vote-up-{{$battleplan->id}}" onclick="vote(1,{{$battleplan->id}}, this)" data-toggle="tooltip" data-placement="top" title="Up Vote"></i>
-            @else
-                <i class="fas fa-arrow-circle-up cursor-click" id="vote-up-{{$battleplan->id}}" onclick="vote(1,{{$battleplan->id}}, this)" data-toggle="tooltip" data-placement="top" title="Up Vote"></i>
-            @endif
-
-            |
-
-            @if ($battleplan->voted(-1))
-                <i class="fas fa-arrow-circle-down cursor-click vote-red" id="vote-down-{{$battleplan->id}}" onclick="vote(-1,{{$battleplan->id}}, this)" data-toggle="tooltip" data-placement="top" title="Down Vote"></i>
-            @else
-                <i class="fas fa-arrow-circle-down cursor-click" id="vote-down-{{$battleplan->id}}" onclick="vote(-1,{{$battleplan->id}}, this)" data-toggle="tooltip" data-placement="top" title="Down Vote"></i>
-            @endif
-
-            |
-
-            <i class="fas fa-clone cursor-click" id="copy-{{$battleplan->id}}" data-toggle="tooltip" data-placement="top" title="Copy to my account" onclick="copyModal({{$battleplan->id}})"></i>
-
+        @if ($battleplan->voted(1))
+        <i class="fas fa-arrow-circle-up cursor-click vote-green" id="vote-up-{{$battleplan->id}}" onclick="vote(1,{{$battleplan->id}}, this)"
+            data-toggle="tooltip" data-placement="top" title="Up Vote"></i>
         @else
-            <small style="color:red">Please sign in to unlock more features</small>
+        <i class="fas fa-arrow-circle-up cursor-click" id="vote-up-{{$battleplan->id}}" onclick="vote(1,{{$battleplan->id}}, this)"
+            data-toggle="tooltip" data-placement="top" title="Up Vote"></i>
         @endif
 
-        
+        |
+
+        @if ($battleplan->voted(-1))
+        <i class="fas fa-arrow-circle-down cursor-click vote-red" id="vote-down-{{$battleplan->id}}" onclick="vote(-1,{{$battleplan->id}}, this)"
+            data-toggle="tooltip" data-placement="top" title="Down Vote"></i>
+        @else
+        <i class="fas fa-arrow-circle-down cursor-click" id="vote-down-{{$battleplan->id}}" onclick="vote(-1,{{$battleplan->id}}, this)"
+            data-toggle="tooltip" data-placement="top" title="Down Vote"></i>
+        @endif
+
+        |
+
+        <i class="fas fa-clone cursor-click" id="copy-{{$battleplan->id}}" data-toggle="tooltip" data-placement="top"
+            title="Copy to my account" onclick="copyModal({{$battleplan->id}})"></i>
+
+        @else
+        <small style="color:red">Please sign in to unlock more features</small>
+        @endif
+
+
     </div>
-</div>
+</div> --}}
 
 <div class="row text-center">
     <div class="col-12 floorChanger">
-        <button type="button" name="button" class="btn btn-primary sidebar-btn" onclick="app.engine.changeFloor(-1)" data-toggle="tooltip" data-placement="top" title="Keybind: Down arrow">Floor
+        <button type="button" name="button" class="btn btn-primary sidebar-btn" onclick="app.engine.changeFloor(-1)"
+            data-toggle="tooltip" data-placement="top" title="Keybind: Down arrow">Floor
             &darr;</button>
-        <button type="button" name="button" class="btn btn-primary sidebar-btn" onclick="app.engine.changeFloor(1)" data-toggle="tooltip" data-placement="top" title="Keybind: Up arrow">Floor
+        <button type="button" name="button" class="btn btn-primary sidebar-btn" onclick="app.engine.changeFloor(1)"
+            data-toggle="tooltip" data-placement="top" title="Keybind: Up arrow">Floor
             &uarr;</button>
-    </div>    
+    </div>
 </div>
 <div class="row">
     {{-- Engine --}}
     <div class="row bg-dark" id="EngineContainer">
         <div id="viewport">
             <canvas id="background" class="fixed"></canvas>
-            <canvas id="overlay" class="fixed" onmouseleave="app.engine.canvasLeave(event)" onmouseenter="app.engine.canvasEnter(event)" onmousemove="app.engine.canvasMove(event)" onmousedown="app.engine.canvasDown(event)" onmouseup="app.engine.canvasUp(event)"
-            onmousedown="app.engine.canvasDown(event)" ondrop="app.engine.canvasDrop(event)" ondragover="app.engine.allowDrop(event)">
+            <canvas id="overlay" class="fixed" onmouseleave="app.engine.canvasLeave(event)" onmouseenter="app.engine.canvasEnter(event)"
+                onmousemove="app.engine.canvasMove(event)" onmousedown="app.engine.canvasDown(event)" onmouseup="app.engine.canvasUp(event)"
+                onmousedown="app.engine.canvasDown(event)" ondrop="app.engine.canvasDrop(event)" ondragover="app.engine.allowDrop(event)">
             </canvas>
         </div>
         <div id="snackbar"></div>
@@ -159,9 +271,11 @@
         @foreach ($battleplan->slots as $key => $slot)
         <div class="row">
             @if (!$slot->operator || !$slot->operator->exists)
-                <input type="image" id="operatorSlot-{{$slot->id}}" src="/media/ops/empty.png" class="op-icon operator-slot operator-border" style="border-color: black" />
+            <input type="image" id="operatorSlot-{{$slot->id}}" src="/media/ops/empty.png" class="op-icon operator-slot operator-border"
+                style="border-color: black" />
             @else
-                <input type="image" id="operatorSlot-{{$slot->id}}" src="{{$slot->operator->icon}}" class="op-icon operator-slot operator-border" style="border-color: #{{$slot->operator->colour}}" />
+            <input type="image" id="operatorSlot-{{$slot->id}}" src="{{$slot->operator->icon}}" class="op-icon operator-slot operator-border"
+                style="border-color: #{{$slot->operator->colour}}" />
             @endif
         </div>
         @endforeach
@@ -171,27 +285,27 @@
 
 @push('modals')
 
-    <div class="modal" id="copy" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Copy Battleplan</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <input type="hidden" id="copy-id">
-                    <h2>Save battleplan as</h2>
-                    <input class="col-4 form-control inline col-12" id="battleplan_name" value="" type="text">
-                
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" name="button" class="btn btn-success" onclick="copy()">Copy</button>
-                </div>
+<div class="modal" id="copy" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Copy Battleplan</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <input type="hidden" id="copy-id">
+                <h2>Save battleplan as</h2>
+                <input class="col-4 form-control inline col-12" id="battleplan_name" value="" type="text">
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" name="button" class="btn btn-success" onclick="copy()">Copy</button>
             </div>
         </div>
     </div>
+</div>
 
 @endpush
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -47,6 +47,16 @@
       integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
       crossorigin="anonymous">
     </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-92408401-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-92408401-2');
+    </script>
+
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
     <script type="text/javascript">
       $.ajaxSetup({
