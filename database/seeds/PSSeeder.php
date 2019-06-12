ef78dbefa6073cdab740bfaaa1392bdf2b40271a
Jared Snape <jaredsnape@gmail.com>
2019-06-11 22:57:17 -0400

    Made Nokk's name have the dope symbol

--- database/seeds/PSSeeder.php
+++ database/seeds/PSSeeder.php
@@ -16,14 +16,14 @@ class PSSeeder extends Seeder
     {
       $opArray = [
         /*Warden*/['name'=> "Warden", 'icon'=> "/media/ops/Warden.png", 'colour'=> "2c3e8c", 'atk'=> false],
-        /*Nokk*/['name'=> "Nokk", 'icon'=> "/media/ops/Nokk.png", 'colour'=> "2c3e8c", 'atk'=> true]
+        /*Nokk*/['name'=> "Nøkk", 'icon'=> "/media/ops/Nokk.png", 'colour'=> "2c3e8c", 'atk'=> true]
       ];
 
       Operator::insert($opArray);
 
       $toolArray = [
         /*Warden*/['name'=> "Warden", 'icon'=> "/media/tools/unique/Warden.png", 'prime'=> true, 'general'=> false],
-        /*Nokk*/['name'=> "Nokk", 'icon'=> "/media/tools/unique/Nokk.png", 'prime'=> true, 'general'=> false],
+        /*Nokk*/['name'=> "Nøkk", 'icon'=> "/media/tools/unique/Nokk.png", 'prime'=> true, 'general'=> false],
       ];
 
       Gadget::insert($toolArray);
